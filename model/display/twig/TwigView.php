5087de20f127ac39af7f3f5f879eeacbf4abe236
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2011-01-21 16:23:56 +0000

    Added TwigView

---
+++ model/display/twig/TwigView.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * TwigView view wrapper
+ *
+ * @author Linus Norton <linusnorton@gmail.com>
+ */
+class TwigView extends View {
+
+    /**
+     * @var Twig
+     */
+    private $twig;
+    private $model;
+
+    /**
+     * Constructor sets up the PHPTAL object
+     */
+    public function  __construct() {
+        parent::__construct(APP_DIR."view".DIRECTORY_SEPARATOR, ".html");
+
+        try {
+            require_once '/path/to/lib/Twig/Autoloader.php';
+            Twig_Autoloader::register();
+        }
+        catch(FrameEx $ex) {
+            $ex->setMessage("Twig not installed");
+            throw $ex;
+        }
+
+        $this->model = array();
+        $this->twig = new Twig_Environment(
+            new Twig_Loader_Filesystem($root."view".DIRECTORY_SEPARATOR),
+            array(
+                'cache' => sys_get_temp_dir(),
+                'debug' => false,
+                'auto_reload' => Registry::get("AUTO_REBUILD_TWIG")
+            )
+        );
+    }
+
+    /**
+     * Add data to the PHPTAL view
+     * @param mixed $data
+     * @param mixed $key
+     */
+    public function add($data, $key = null) {
+        if ($key != null) {
+            $this->model[$key] = $data;
+        }
+    }
+
+    /**
+     * Use PHPTAL to generate some XHTML
+     * @return string
+     */
+    public function execute() {
+        $template = $this->twig->loadTemplate($this->template);
+        return $template->render($this->model);
+    }
+
+    /**
+     * Fall back to a default error view.
+     * @return string
+     */
+    public function getErrorPage() {
+        $this->template = ROOT.Registry::get("ERROR_VIEW");
+        return $this->execute();
+    }
+
+    /**
+     * Pass the magic set on to PHPTAL
+     * @param string $key
+     * @param mixed $value
+     */
+    public function __set($key, $value) {
+        $this->add($value, $key);
+    }
+}
+
