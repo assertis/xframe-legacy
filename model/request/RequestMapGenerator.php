550b79fb26c61c42e31a83a0e664cebd7b3130d6
linusnorton <linusnorton@gmail.com>
2013-11-05 09:14:13 +0000

    Another vendor ignore

--- model/request/RequestMapGenerator.php
+++ model/request/RequestMapGenerator.php
@@ -23,7 +23,7 @@ class RequestMapGenerator {
         //for each file in the directory
         while (($file = readdir($dh)) !== false) {
             //if it is something we want to ignore...
-            if ($file == "test" || $file == "." || $file == ".."  || $file == ".svn") {
+            if ($file == "test" || $file == "." || $file == ".."  || $file == ".svn" || $file == "vendor") {
                 continue;
             }
             //if it is a directory...
