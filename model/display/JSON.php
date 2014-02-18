78dd8c1d615c94c06f69c3e9671cf415e0e3881a
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2014-02-18 14:53:49 +0000

    JSON view

---
+++ model/display/JSON.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ */
+class JSON extends View {
+    private $data = array();
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function execute() {
+        if (!headers_sent()) {
+            header("content-type: application/json");
+        }
+        return json_encode($this->data);
+    }
+
+    public function getErrorPage() {
+
+    }
+
+    public function set($data) {
+        $this->data = $data;
+    }
+
+    public function add($data, $key = null) {
+        if ($key == null)
+            $this->data[] = $data;
+        else
+            $this->data[$key] = $data;
+    }
+
+    public function addStaticInclude($path) {
+
+    }
+
+}
