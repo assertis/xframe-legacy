15331825ae61c1b062438911dce196195351d75b
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2014-03-03 16:12:09 +0000

    Revert "Revert "Merge pull request #1 from assertis/rest""
    This reverts commit 2f13bf6feec6d9fd7e22034f98159b76efe4811a.

--- www/index.php
+++ www/index.php
@@ -8,6 +8,6 @@
 
 require(dirname(__FILE__).'/../init.php');
 //pass the request URI, parameters and path of this file to the request
-$request = new Request($_SERVER["REQUEST_URI"], $_REQUEST, $_SERVER["PHP_SELF"]);
+$request = new Request($_SERVER['REQUEST_URI'], $_REQUEST, $_SERVER['PHP_SELF'], $_SERVER['REQUEST_METHOD']);
 //pass request to the dispatcher which maps it to a resource and controller 
 echo Dispatcher::dispatch($request);
