4ed4016fc76829ce50cfe9d10f7a9b2ef53e9b0e
Linus Norton <linusnorton@gmail.com>
2014-07-17 11:23:58 +0100

    Added PUT support

--- www/index.php
+++ www/index.php
@@ -7,6 +7,10 @@
  */
 
 require(dirname(__FILE__).'/../init.php');
+
+parse_str(file_get_contents("php://input"), $_PUT);
+$_REQUEST = array_merge($_REQUEST, $_PUT);
+
 //pass the request URI, parameters and path of this file to the request
 $request = new Request($_SERVER['REQUEST_URI'], $_REQUEST, $_SERVER['PHP_SELF'], $_SERVER['REQUEST_METHOD']);
 //pass request to the dispatcher which maps it to a resource and controller 
