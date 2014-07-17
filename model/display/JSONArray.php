ce1fe6a66e98eed2a41ed6d8d5858386dfdc9053
Linus Norton <linusnorton@gmail.com>
2014-07-17 10:31:57 +0100

    Added JSONArray interface

---
+++ model/display/JSONArray.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Linus Norton <linusnorton@gmail.com>
+ *
+ * @package display
+ *
+ */
+interface JSONArray {
+
+    /**
+     * Return me as an array
+     *
+     * @return array
+     */
+    public function toArray();
+
+}
