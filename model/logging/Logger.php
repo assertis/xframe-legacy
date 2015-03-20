7c445d71467bb403b30b5f3d034006ec0f8380a4
Maciej Romanski <maciej.romanski@assertis.co.uk>
2015-03-20 14:10:52 +0100

    Make logger more not related on server configuration.

--- model/logging/Logger.php
+++ model/logging/Logger.php
@@ -1,7 +1,4 @@
 <?php
-use Monolog\Formatter\LogstashFormatter;
-use Monolog\Handler\RedisHandler;
-use Monolog\Logger as MonologLogger;
 
 /**
  * @author Linus Norton <linus.norton@assertis.co.uk>, Jason Paige <jason.paige@assertis.co.uk>
@@ -17,49 +14,51 @@ class Logger {
     private $tableName;
     private $logLevel;
     private $logFile;
+
     /**
-     * @var MonologLogger
+     * @var \Monolog\Logger
      */
     private $client;
-
-    private static $loggerToMonologCodes = [
-        self::DEBUG => MonologLogger::DEBUG,
-        self::INFO => MonologLogger::INFO,
-        self::WARN => MonologLogger::WARNING,
-        self::ERROR => MonologLogger::ERROR,
-        self::AUDIT => MonologLogger::ERROR,
-        self::FATAL => MonologLogger::CRITICAL,
-    ];
-
-    private static $codeNamesToMonologCode = [
-        'debug' => MonologLogger::DEBUG,
-        'info' => MonologLogger::INFO,
-        'warning' => MonologLogger::WARNING,
-        'error' => MonologLogger::ERROR,
-        'audit' => MonologLogger::ERROR,
-        'fatal' => MonologLogger::CRITICAL,
-    ];
 
     public function __construct($key) {
         $this->key = $key;
         $this->tableName = (Registry::get("LOG_TABLE")) ? Registry::get("LOG_TABLE") : "log";
+        $this->logLevel = (Registry::get("LOG_LEVEL")) ? Registry::get("LOG_LEVEL") : self::OFF;
 
-        $systemName = (Registry::get("RESOURCE_SITE")) ? Registry::get("RESOURCE_SITE") : "XFRAME";
-        $applicationName = (Registry::get("DATABASE_NAME")) ? Registry::get("DATABASE_NAME") : null;
-        $logStashKey = (Registry::get("LOG_TABLE")) ? Registry::get("LOG_TABLE") : "log";
+        try {
+            $this->initMonolog($key, $this->logLevel);
+        } catch (\Exception $e) {
+            //Nothing
+        }
+    }
 
-        $logLevel = (Registry::get("LOG_LEVEL")) ? Registry::get("LOG_LEVEL") : self::OFF;
-        $this->logLevel = $logLevel;
+    private function initMonolog($key, $logLevel) {
+        if (!class_exists('\Redis') || !class_exists('\Monolog\Logger')) {
+            return null;
+        }
 
         $redisHost = (Registry::get("REDIS_HOST")) ? Registry::get("REDIS_HOST") : '127.0.0.1';
         $redisPort = (Registry::get("REDIS_PORT")) ? Registry::get("REDIS_PORT") : 6379;
         $redisTimeout = (Registry::get("REDIS_TIMEOUT")) ? Registry::get("REDIS_TIMEOUT") : 0.0;
 
-        $this->client = new MonologLogger($key);
-        $redis = new Redis();
+        $systemName = (Registry::get("RESOURCE_SITE")) ? Registry::get("RESOURCE_SITE") : "XFRAME";
+        $applicationName = (Registry::get("DATABASE_NAME")) ? Registry::get("DATABASE_NAME") : null;
+        $logStashKey = (Registry::get("LOG_TABLE")) ? Registry::get("LOG_TABLE") : "log";
+
+        $codes = [
+            self::DEBUG => \Monolog\Logger::DEBUG,
+            self::INFO => \Monolog\Logger::INFO,
+            self::WARN => \Monolog\Logger::WARNING,
+            self::ERROR => \Monolog\Logger::ERROR,
+            self::AUDIT => \Monolog\Logger::ERROR,
+            self::FATAL => \Monolog\Logger::CRITICAL,
+        ];
+
+        $this->client = new \Monolog\Logger($key);
+        $redis = new \Redis();
         $redis->connect($redisHost, $redisPort, $redisTimeout);
-        $handler = new RedisHandler($redis, $logStashKey, self::getCode($logLevel, self::$loggerToMonologCodes));
-        $handler->setFormatter(new LogstashFormatter($applicationName, $systemName, null, "_", LogstashFormatter::V1));
+        $handler = new \Monolog\Handler\RedisHandler($redis, $logStashKey, self::getCode($logLevel, $codes));
+        $handler->setFormatter(new \Monolog\Formatter\LogstashFormatter($applicationName, $systemName, null, "_", \Monolog\Formatter\LogstashFormatter::V1));
         $this->client->pushHandler($handler);
     }
 
@@ -75,7 +74,11 @@ class Logger {
             return $codes[$nameOrCode];
         }
 
-        return MonologLogger::INFO;
+        try {
+            return \Monolog\Logger::INFO;
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 
     /**
@@ -103,49 +106,69 @@ class Logger {
      * Log a debug message (dependant on the level of logging)
      */
     public function debug($message) {
-        if ($this->logLevel >= self::DEBUG){
+        if ($this->logLevel >= self::DEBUG) {
             $this->log("debug", $message);
         }
     }
 
     public function info($message) {
-        if ($this->logLevel >= self::INFO){
+        if ($this->logLevel >= self::INFO) {
             $this->log("info", $message);
         }
     }
 
     public function warn($message) {
-        if ($this->logLevel >= self::WARN){
+        if ($this->logLevel >= self::WARN) {
             $this->log("warn", $message);
         }
     }
 
     public function audit($message) {
-        if ($this->logLevel >= self::AUDIT){
+        if ($this->logLevel >= self::AUDIT) {
             $this->log("audit", $message);
         }
     }
 
     public function error($message) {
-        if ($this->logLevel >= self::ERROR){
+        if ($this->logLevel >= self::ERROR) {
             $this->log("error", $message);
         }
     }
 
     public function fatal($message) {
-        if ($this->logLevel >= self::FATAL){
+        if ($this->logLevel >= self::FATAL) {
             $this->log("fatal", $message);
         }
     }
 
+    private function logUsingMonlog($level, $message) {
+        if (empty($this->client)) {
+            return null;
+        }
+
+        $codes = [
+            'debug' => \Monolog\Logger::DEBUG,
+            'info' => \Monolog\Logger::INFO,
+            'warning' => \Monolog\Logger::WARNING,
+            'error' => \Monolog\Logger::ERROR,
+            'audit' => \Monolog\Logger::ERROR,
+            'fatal' => \Monolog\Logger::CRITICAL,
+        ];
+
+        $this->client->log(self::getCode($level, $codes), $message);
+    }
+
     private function log($level, $message) {
-        //Log to logstash
-        $this->client->log(self::getCode($level, self::$codeNamesToMonologCode), $message);
+        try {
+            //Log to logstash
+            $this->logUsingMonlog($level, $message);
+        } catch (\Exception $e) {
+            //Nothing
+        }
 
         if ($this->logFile != null) {
             $this->logTofile($message);
-        }
-        else {
+        } else {
             $this->logToDatabase($level, $message);
         }
     }
@@ -153,7 +176,7 @@ class Logger {
     private function logToFile($message) {
         $fp = fopen($this->logFile, 'a');
         if ($fp) {
-            fwrite($fp, $message."\n");
+            fwrite($fp, $message . "\n");
             fclose($fp);
             @chmod($this->logFile, 0775);
         }
@@ -166,15 +189,12 @@ class Logger {
         //check ip from share internet
         if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
             $ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-        }
-        else if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        } else if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-        //to check ip is pass from proxy
+        } //to check ip is pass from proxy
         else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        }
-        else {
+        } else {
             $ip = $_SERVER['REMOTE_ADDR'];
         }
 
